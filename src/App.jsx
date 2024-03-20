1a714a3e37f46eea5726386fb7f584d2a10fb015
Thiruvalappagari Sumithra <sumithrat49@gmail.com>
2024-03-20 11:24:32 +0530

    quiz

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Quiz from './components/Quiz';
+
+const App = () => {
+  return (
+    <>
+    <div>
+      <Quiz/>
+      </div></>
+  )
+}
+
+export default App
